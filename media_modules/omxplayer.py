45b3e849c22233dad8a7cf432925f2223bf8560b
clacktronics <clacktronics@gmail.com>
2015-05-10 12:28:19 +0000

    Fulscreen function added, can stretch, fill, fit screen or be actual resolution

--- media_modules/omxplayer.py
+++ media_modules/omxplayer.py
@@ -2,10 +2,12 @@
 
 from time import sleep
 import subprocess,dbus,getpass,os
-from re import match
+from re import match, search
 
 class media():
 	def __init__(self,media,**kwargs):
+		self.media = media
+
 		try:
 			self.nick = match('[0-9A-Za-z]*', media).group(0)
 		except:
@@ -59,6 +61,7 @@ class media():
 		pass
 	def unpaused():
 		pass
+
 	def quit(self):
 		self.object.Quit()
 
@@ -80,17 +83,102 @@ class media():
 		if 'kill' in args:
 			self.quit()
 	
-	# Visual Video actions
+	# Video actions
 
 	def resize(self,x1,y1,x2,y2):
 		pos = '%s %s %s %s' % (str(x1),str(y1),str(x2),str(y2))
 		self.PlayerInter.VideoPos(dbus.ObjectPath('/not/used'),dbus.String(pos))
 		print 'set pos to ' + pos
 
+	def getRes(self):
+
+		# Use Raspberry Pi's built in screen program to get resolution
+		tvservice = subprocess.Popen(['tvservice','-s'], stdout=subprocess.PIPE)
+		tvstatus, err = tvservice.communicate()
+		screenres = search(' ([0-9]*)x([0-9]*) ',tvstatus)
+		self.screenX,self.screenY = int(screenres.group(1)), int(screenres.group(2))
+
+		# Get movies info from another instance of omx
+		omx = subprocess.Popen(['omxplayer','-i', self.media], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out,err = omx.communicate()
+		vidres = search(' ([0-9]*)x([0-9]*) ',err)
+		self.videoX,self.videoY = int(vidres.group(1)),int(vidres.group(2))
+		
+		#print 'screen %dx%d , video%dx%d' % (self.screenX,self.screenY,self.videoX,self.videoY)
+
+	def fullscreen(self,type):
+		# fit and fill are not exact due to rounding floats
+		self.getRes()
+		sratio = self.screenX / float(self.screenY)
+		vratio = self.videoX / float(self.videoY)
+
+		if type == "fit":
+			if sratio == vratio:
+				self.resize(0,0,self.screenX,self.screenY)
+			if sratio > vratio:
+				xwidth = self.videoX * (self.screenY / float(self.videoY))
+				xoffset = (self.screenX - xwidth) / 2
+				x1 = int(xoffset)
+				x2 = int(self.screenX - xoffset)
+				self.resize(x1,0,x2,self.screenY)
+			if sratio < vratio:
+				vheight = self.videoY * (self.screenX / float(self.videoX))
+				voffset = (self.screenY - vheight) / 2
+				y1 = int(voffset)
+				y2 = int(self.screenY - voffset)
+				self.resize(0,y1,self.screenX,y2)
+		elif type == "fill":
+			if sratio == vratio:
+				self.resize(0,0,self.screenX,self.screenY)
+			elif sratio > vratio:
+				# calculate overflow top and bottom
+				vheight = self.videoY * (self.screenX / float(self.videoX))
+				voffset = (vheight - self.screenY) / 2
+				y1 = int(0 - voffset)
+				y2 = int(self.screenY + voffset)
+
+				self.resize(0,y1,self.screenX,y2)
+
+			elif sratio < vratio:
+				# Calculate overflow on sides
+				xwidth = self.videoX * (self.screenY / float(self.videoY))
+				xoffset = (xwidth - self.screenX) / 2
+				x1 = int(0 - xoffset)
+				x2 = int(self.screenX + xoffset)
+
+				self.resize(x1,0,x2,self.screenY)
+
+			else: print 'not changed s=%f v=%f' % (sratio,vratio) 	
+		elif type == "stretch":
+			self.resize(0,0,self.screenX,self.screenY)
+		elif type == "actual":
+			x1 = (self.screenX - self.videoX) / 2
+			x2 = x1 + self.videoX
+			y1 = (self.screenY - self.videoY) / 2
+			y2 = y1 + self.videoY
+			self.resize(x1,y1,x2,y2)
+		else: print "Sorry, command not understood, use 'fit','fill' or 'stretch'"
+
 	def alpha(self,alpha):
 		self.PlayerInter.SetAlpha(dbus.ObjectPath('/not/used'),dbus.Int64(alpha))
 		#print 'Alpha  set to %s' % alpha
 	
+	def wipe(self,type):
+		pass
+		# shift(left/right,up/down)
+		# fold(left/right/up/down)
+		# shrink to point
+		# shrink to line
+		# expand to mass
+		# expand widt
+		
+	def animate(self,type):
+		pass
+		# ping pong
+		# Spiral grow
+		# shudder
+		# flicker
 
-		
-		
+	def dissolve():
+		pass
+		#alpha fadeout
--- test.py
+++ test.py
@@ -1,28 +1,19 @@
 from subprocess import call
 from time import sleep
 from media_modules import omxplayer
-vid1 = omxplayer.media('stvincent.mp4',vol = '-6000', layer = '2')
-vid2 = omxplayer.media('boy.mp4',vol = '-6000',layer = '1')
+vid1 = omxplayer.media('future.mp4',vol = '-6000', layer = '2')
  
 vid1.cue()
-vid2.cue()
 
 sleep(1)
 
 vid1.pause()
-vid2.pause()
-
-vid1.fade('in',.5)
-
-sleep(1)
-x1 = 0
-x2 = 1280
-#for i in range(1281):
-#	vid1.resize(x1,0,x2,720)
-#	sleep(.01)
-#	x1 += 1
-#	x2 += 1
 while True:
-	for i in range(0,256,10):
-		vid1.alpha(i)
-vid1.fade('out',10,'kill')
+#	vid1.fullscreen('stretch')
+	sleep(5)
+	vid1.fullscreen('actual')
+#	sleep(5)
+#	vid1.fullscreen('fill')
+	sleep(5)
+	vid1.fullscreen('fit')
+	sleep(5)
